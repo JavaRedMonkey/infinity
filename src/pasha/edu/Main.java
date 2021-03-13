7c811968a7b13289b99568f9b2853403966c254d
Java02 <alekseenkooleg2004@gmail.com>
2021-03-13 13:24:05 +0200

    infinity max value update

--- src/pasha/edu/Main.java
+++ src/pasha/edu/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         while (true){
-            System.out.println(random.nextInt(10000));
+            System.out.println(random.nextInt(Integer.MAX_VALUE));
             Thread.sleep(1000);
         }
     }
